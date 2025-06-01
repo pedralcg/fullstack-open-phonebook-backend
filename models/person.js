8e68af9ad62c6bb588ef2f6c6c711cd291015475
Pedro Alcoba <pedralcg@gmail.com>
2025-06-01 17:55:44 +0200

    Part03c - Ejer 3.20* BBDD Phonebook, paso 8

--- models/person.js
+++ models/person.js
@@ -18,13 +18,28 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 3,
+    minlength: 3, // Asegúrate de que el nombre tenga al menos 3 caracteres
     required: true,
-    unique: true
+    unique: true // Si quieres que los nombres sean únicos, como se ha discutido en ejercicios anteriores
   },
   number: {
     type: String,
-    required: true
+    // La primera parte de la validación: longitud mínima de 8 caracteres
+    minlength: 8,
+    required: true,
+    // Definimos el validador personalizado
+    validate: {
+      validator: function(v) {
+        // Expresión regular para el formato:
+        // ^\d{2,3}  -> Inicia con 2 o 3 dígitos
+        // -         -> Seguido de un guion
+        // \d+$      -> Seguido de uno o más dígitos hasta el final de la cadena
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!. 
+                        A phone number must have 2 or 3 numbers in the first part,
+                        followed by '-' and then numbers.`
+    }
   }
 })
 
